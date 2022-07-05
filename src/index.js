1e5c0c721feb3b7bb4719e2347110b1d77e7c988
siiine-764 <yassinepro764@gmail.com>
2022-07-05 15:38:03 +0100

    ich verankommen

--- src/index.js
+++ src/index.js
@@ -49,7 +49,9 @@ class Navbar extends React.Component {
             <li>
             </li>
             <li>
-            <a href="https://drive.google.com/file/d/1QIdPkUV0iBiBp2tvUX-hdUR1-sFbKkLk/view?usp=sharing" class="link" id="btnresume"> Resume </a> </li>
+            <button onclick="location.href='https://drive.google.com/file/d/1QIdPkUV0iBiBp2tvUX-hdUR1-sFbKkLk/view?usp=sharing'" type="button"  class="link" id="btnresume">
+         Resume</button> 
+         </li>
             <li><button class="link" id="btnblog"  href="#blog">Blog</button></li>
           </ul>
         </nav>
